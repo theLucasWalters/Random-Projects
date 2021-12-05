97a68a5a4ed0ccbb7dd1f6fdbf2177a19cbe0675
Lucas Walters <lwalters224@gmail.com>
2021-12-04 22:21:30 -0700

    updated to `if __name__ =='__main__'` format

--- sort-chars.py
+++ sort-chars.py
@@ -1,29 +1,33 @@
-vowels = []
-consonants = []
-integers = []
-others = []
-spaces = 0
+def main():
+    vowels = []
+    consonants = []
+    integers = []
+    others = []
+    spaces = 0
 
-all_vowels = 'AEIOUaeiou'
-all_consonants = 'BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnopqrstvwxyz'
-all_ints = '0123456789'
+    all_vowels = 'AEIOUaeiou'
+    all_consonants = 'BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnopqrstvwxyz'
+    all_ints = '0123456789'
 
-string = input('> ')
+    string = input('> ')
 
-for char in string:
-    if char in all_vowels:
-        vowels.append(char)
-    elif char in all_consonants:
-        consonants.append(char)
-    elif char in all_ints:
-        integers.append(char)
-    elif char == ' ':
-        spaces += 1
-    else:
-        others.append(char)
+    for char in string:
+        if char in all_vowels:
+            vowels.append(char)
+        elif char in all_consonants:
+            consonants.append(char)
+        elif char in all_ints:
+            integers.append(char)
+        elif char == ' ':
+            spaces += 1
+        else:
+            others.append(char)
 
-print(vowels)
-print(consonants)
-print(integers)
-print(others)
-print(f'{spaces} spaces')
+    print(vowels)
+    print(consonants)
+    print(integers)
+    print(others)
+    print(f'{spaces} spaces')
+
+if __name__ == "__main__":
+    main()
